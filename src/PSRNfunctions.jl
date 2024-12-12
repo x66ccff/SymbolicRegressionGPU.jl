bdafd38dcca59ed43833a8d5c7e30180aa4adb8d
x66ccff <2313788275@qq.com>
2024-12-12 16:33:59 +0800

    make PSRNfunction more simple

--- src/PSRNfunctions.jl
+++ src/PSRNfunctions.jl
@@ -3,105 +3,61 @@ module PSRNfunctions
 using KernelAbstractions
 const KA = KernelAbstractions
 
-# unary operator kernels
-@kernel function identity_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = x[I]
+# Generator for unary operator kernels
+function generate_unary_kernel(op::F, name::Symbol) where {F<:Function}
+    @eval begin
+        @kernel function $name(out, x)
+            I = @index(Global)
+            @inbounds out[I] = $op(x[I])
+        end
+    end
 end
 
-@kernel function neg_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = -x[I]
+# Generator for binary operator kernels
+function generate_binary_kernel(op::F, name::Symbol) where {F<:Function}
+    @eval begin
+        @kernel function $name(out, x1, x2)
+            I = @index(Global)
+            @inbounds out[I] = $op(x1[I], x2[I])
+        end
+    end
 end
 
-@kernel function inv_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = 1 / x[I]
-end
+# Define kernels using generators
+generate_unary_kernel(identity, :identity_kernel!)
+generate_unary_kernel(-, :neg_kernel!)
+generate_unary_kernel(x -> 1/x, :inv_kernel!)
+generate_unary_kernel(sin, :sin_kernel!)
+generate_unary_kernel(cos, :cos_kernel!)
+generate_unary_kernel(exp, :exp_kernel!)
+generate_unary_kernel(x -> log(abs(x)), :log_kernel!)
+generate_unary_kernel(sqrt, :sqrt_kernel!)
 
-@kernel function sin_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = sin(x[I])
-end
+generate_binary_kernel(+, :add_kernel!)
+generate_binary_kernel(*, :mul_kernel!)
+generate_binary_kernel(/, :div_kernel!)
+generate_binary_kernel(-, :sub_kernel!)
+generate_binary_kernel(^, :pow_kernel!)
 
-@kernel function cos_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = cos(x[I])
-end
-
-@kernel function exp_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = exp(x[I])
-end
-
-@kernel function log_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = log(abs(x[I]))
-end
-
-# binary operator kernels
-@kernel function add_kernel!(out, x1, x2)
-    I = @index(Global)
-    @inbounds out[I] = x1[I] + x2[I]
-end
-
-@kernel function mul_kernel!(out, x1, x2)
-    I = @index(Global)
-    @inbounds out[I] = x1[I] * x2[I]
-end
-
-@kernel function div_kernel!(out, x1, x2)
-    I = @index(Global)
-    @inbounds out[I] = x1[I] / x2[I]
-end
-
-@kernel function sub_kernel!(out, x1, x2)
-    I = @index(Global)
-    @inbounds out[I] = x1[I] - x2[I]
-end
-
-@kernel function pow_kernel!(out, x1, x2)
-    I = @index(Global)
-    @inbounds out[I] = x1[I] ^ x2[I]
-end
-
-@kernel function sqrt_kernel!(out, x)
-    I = @index(Global)
-    @inbounds out[I] = sqrt(x[I])
-end
-
-# execution functions
+# 执行函数保持不变
 function execute_unary_op(kernel!, x, backend)
-    # select appropriate workgroup size based on backend
     workgroup_size = backend isa KA.CPU ? 64 : 256
-    
-    # create output array
     out = similar(x)
-    
-    # create and execute kernel
     kernel = kernel!(backend, workgroup_size)
     event = kernel(out, x, ndrange=size(x))
     wait(event)
-    
     return out
 end
 
 function execute_binary_op(kernel!, x1, x2, backend)
-    # select appropriate workgroup size based on backend
     workgroup_size = backend isa KA.CPU ? 64 : 256
-    
-    # create output array
     out = similar(x1)
-    
-    # create and execute kernel
     kernel = kernel!(backend, workgroup_size)
     event = kernel(out, x1, x2, ndrange=size(x1))
     wait(event)
-    
     return out
 end
 
-# export all kernels and execution functions
 export identity_kernel!, add_kernel!, mul_kernel!, div_kernel!, sub_kernel!,
        neg_kernel!, inv_kernel!, sin_kernel!, cos_kernel!, exp_kernel!,
        log_kernel!, pow_kernel!, sqrt_kernel!, execute_unary_op, execute_binary_op
